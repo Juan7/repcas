5284690ca5265cb36c7e2e32f7c97db6f17df73a
Yoel <mack.zalas@gmail.com>
2018-04-22 16:01:10 -0500

    add base viewsets

--- repcas/accounting/admin.py
+++ repcas/accounting/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.InvoiceItem)
+class InvoiceItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Quotation)
+class QuotationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.QuotationItem)
+class QuotationItemAdmin(admin.ModelAdmin):
+    pass
---
+++ repcas/accounting/api.py
@@ -0,0 +1,18 @@
+import django_filters
+from rest_framework import permissions, response, status, viewsets, filters
+
+from main import pagination
+from . import models, serializers
+
+
+class UserViewSet(viewsets.ModelViewSet):
+
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = serializers.InvoiceSerializer
+
+    pagination_class = pagination.StandardResultsSetPagination
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend, filters.OrderingFilter)
+    queryset = models.Invoice.objects.all()
+    filter_fields = {
+        'number': ['icontains']
+    }
