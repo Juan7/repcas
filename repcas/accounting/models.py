9aa52b5c3586baa9a52ad94d70d7c50b2553c6ea
Juan Gomez <juan.gomez.q@gmail.com>
2018-04-22 15:48:52 -0500

    Complete migrations

--- repcas/accounting/models.py
+++ repcas/accounting/models.py
@@ -5,7 +5,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 
-# Create your models here.
+from accounts.models import Agent, Client
+from inventory.models import Product
 
 class Invoice(models.Model):
     WAITING = 1
