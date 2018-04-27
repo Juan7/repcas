5005886c28c2dc550ad535d320e2cc1d096ff844
Juan7 <juan.gomez.q@gmail.com>
2018-04-27 03:16:29 -0500

    inventory admin

--- repcas/inventory/admin.py
+++ repcas/inventory/admin.py
@@ -6,3 +6,28 @@ from . import models
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(models.Laboratory)
+class LaboratoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ProductDistributionChannel)
+class ProductDistributionChannelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ProductScale)
+class ProductScaleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ProductPromotion)
+class ProductPromotionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.SpecialPrice)
+class SpecialPriceAdmin(admin.ModelAdmin):
+    pass
